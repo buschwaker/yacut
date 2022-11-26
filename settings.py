ff95e96e635911d387dfa0546caf57b6f6f3068f
Александр Крошко <akroshko1995@gmail.com>
2022-11-26 11:34:27 +0300

    settings changed

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'Akroshko_1995')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
